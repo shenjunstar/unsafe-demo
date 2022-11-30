24d44d92c7d0b57709364b30c831ebcd6eb1a978
Jeff.Shen <jeff.shen@mirmiles.com>
2022-11-30 10:54:51 +0800

    初始提交

---
+++ src/main/java/com/wangoon/demo/unsafe/unsafedemo/UnsafeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wangoon.demo.unsafe.unsafedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsafeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnsafeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangoon/demo/unsafe/unsafedemo/UnsafeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangoon.demo.unsafe.unsafedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsafeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
